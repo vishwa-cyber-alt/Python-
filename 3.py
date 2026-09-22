344a17127e89286d52eb9ed69a3d8a744f57e485
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2026-09-22 13:37:32 -0400

    Add conditional statements and user input handling

---
+++ 3.py
@@ -0,0 +1,18 @@
+if(True):
+    print("yes")
+else:
+    print("no")
+#----
+if(False):
+    print("yes")
+else:
+    print("no")
+#----
+print("win"=="win")
+print("win"=="wi")
+#----
+meghna=input("Enter :")
+if meghna=="died":
+    print("surya meets priya")
+else:
+    print("surya weds meghna")
